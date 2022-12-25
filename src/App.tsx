7d4c5660e5d3af61f47ecf586946022e4ebbc2b3
Gitarackur <peterosah5@gmail.com>
2022-12-25 17:01:48 +0100

    A CRUD Application built with refine.dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from "react";
-
+import './index.css';
 import { Refine } from "@pankod/refine-core";
-
 import dataProvider from "@pankod/refine-airtable";
 import routerProvider from "@pankod/refine-react-router-v6";
 import { useTranslation } from "react-i18next";
+import { Layout } from " components/Layout";
+import { PostList } from "pages/post/list";
+import { PostShow } from "pages/post/show";
+import { PostCreate } from "pages/post/create";
+import { PostEdit } from "pages/post/edit";
 
 function App() {
   const { t, i18n } = useTranslation();
 
-  const API_TOKEN = "your-airtable-api-token";
-  const BASE_ID = "your-airtable-base-id";
+  const API_TOKEN = "key0uWArSH56JHNJV";
+  const BASE_ID = "appez0LgaOVA6SdCO";
 
   const i18nProvider = {
     translate: (key: string, params: object) => t(key, params),
@@ -23,6 +27,17 @@ function App() {
       dataProvider={dataProvider(API_TOKEN, BASE_ID)}
       routerProvider={routerProvider}
       i18nProvider={i18nProvider}
+      resources={[
+        { 
+          name: "posts", 
+          icon: '', 
+          list: PostList,
+          show: PostShow,
+          create: PostCreate,
+          edit: PostEdit
+        },
+      ]}
+      Layout={Layout}
     />
   );
 }
